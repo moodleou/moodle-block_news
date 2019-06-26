5155cf81acdb30d6c523a8a6063195aef30cc5e2
John Beedell <john.beedell@open.ac.uk>
2019-06-26 12:33:29 +0100

    Mobile: Events padding adjustments. #342227 #328622

--- db/mobile.php
+++ db/mobile.php
@@ -35,8 +35,8 @@ $addons = [
                     'class' => 'block_news'
                 ],
                 'styles' => [
-                    'url' => $CFG->wwwroot . '/blocks/news/mobile.css?v=2019062400',
-                    'version' => 2019062400
+                    'url' => $CFG->wwwroot . '/blocks/news/mobile.css?v=2019062500',
+                    'version' => 2019062500
                 ],
                 'priority' => 60,
                 'init' => 'news_init'
