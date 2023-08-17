7238a830f0c95ba476c34974dd2a1c0d59c54a1c
Tim Hunt <T.J.Hunt@open.ac.uk>
2023-08-17 16:46:05 +0100

    M4.2: external APIs: update to use new base classes #711061

--- classes/external.php
+++ classes/external.php
@@ -25,25 +25,26 @@
 namespace block_news;
 
 use block_news\output\mobile;
+use core_external\external_api;
+use core_external\external_function_parameters;
+use core_external\external_multiple_structure;
+use core_external\external_single_structure;
+use core_external\external_value;
 
-defined('MOODLE_INTERNAL') || die();
-
-require_once($CFG->libdir . '/externallib.php');
-
-class external extends \external_api {
+class external extends external_api {
 
     /**
      * Define paramters for get_message_page function
      *
-     * @return \external_function_parameters
+     * @return external_function_parameters
      */
     public static function get_message_page_parameters() {
-        return new \external_function_parameters([
-            'courseid' => new \external_value(PARAM_INT, 'Course ID'),
-            'type' => new \external_value(PARAM_INT, 'Message type', VALUE_REQUIRED,
+        return new external_function_parameters([
+            'courseid' => new external_value(PARAM_INT, 'Course ID'),
+            'type' => new external_value(PARAM_INT, 'Message type', VALUE_REQUIRED,
                     message::MESSAGETYPE_NEWS),
-            'pagenum' => new \external_value(PARAM_INT, 'Page number', VALUE_REQUIRED),
-            'pastevents' => new \external_value(PARAM_BOOL, 'Display past events', VALUE_REQUIRED,
+            'pagenum' => new external_value(PARAM_INT, 'Page number', VALUE_REQUIRED),
+            'pastevents' => new external_value(PARAM_BOOL, 'Display past events', VALUE_REQUIRED,
                     false)
         ]);
     }
@@ -77,44 +78,44 @@ class external extends \external_api {
     /**
      * Return definition for get_message_page
      *
-     * @return \external_single_structure
+     * @return external_single_structure
      */
     public static function get_message_page_returns() {
-        return new \external_single_structure([
-            'messages' => new \external_multiple_structure(new \external_single_structure([
-                'isnews' => new \external_value(PARAM_BOOL, 'Is this message news, rather than an event?', VALUE_OPTIONAL, false),
-                'id' => new \external_value(PARAM_INT, 'Message ID'),
-                'messagedate' => new \external_value(PARAM_TEXT, 'Message date'),
-                'title' => new \external_value(PARAM_TEXT, 'Message title', VALUE_OPTIONAL),
-                'author' => new \external_value(PARAM_TEXT, 'Author\'s name', VALUE_OPTIONAL),
-                'imageurl' => new \external_value(PARAM_URL, 'URL of message image',
+        return new external_single_structure([
+            'messages' => new external_multiple_structure(new external_single_structure([
+                'isnews' => new external_value(PARAM_BOOL, 'Is this message news, rather than an event?', VALUE_OPTIONAL, false),
+                'id' => new external_value(PARAM_INT, 'Message ID'),
+                'messagedate' => new external_value(PARAM_TEXT, 'Message date'),
+                'title' => new external_value(PARAM_TEXT, 'Message title', VALUE_OPTIONAL),
+                'author' => new external_value(PARAM_TEXT, 'Author\'s name', VALUE_OPTIONAL),
+                'imageurl' => new external_value(PARAM_URL, 'URL of message image',
                     VALUE_OPTIONAL),
-                'imagewidth' => new \external_value(PARAM_INT, 'Width of message image',
+                'imagewidth' => new external_value(PARAM_INT, 'Width of message image',
                     VALUE_OPTIONAL, 700),
-                'imageheight' => new \external_value(PARAM_INT, 'Height of message image',
+                'imageheight' => new external_value(PARAM_INT, 'Height of message image',
                     VALUE_OPTIONAL, 330),
-                'imagedesc' => new \external_value(PARAM_TEXT, 'Image description', VALUE_OPTIONAL, ''),
-                'eventdate' => new \external_value(PARAM_TEXT, 'Short-form start date of event', VALUE_OPTIONAL),
-                'eventmonth' => new \external_value(PARAM_ALPHA, 'Month of the event start date', VALUE_OPTIONAL),
-                'eventday' => new \external_value(PARAM_ALPHANUM, 'Day of the event start date', VALUE_OPTIONAL),
-                'fulleventdate' => new \external_value(PARAM_TEXT, 'Long-form start date of event', VALUE_OPTIONAL),
-                'eventlocation' => new \external_value(PARAM_TEXT, 'Event location', VALUE_OPTIONAL),
-                'formattedmessage' => new \external_value(PARAM_RAW, 'HTML formatted message text'),
-                'viewlink' => new \external_value(PARAM_URL, 'Internal link to full message', VALUE_OPTIONAL),
-                'link' => new \external_value(PARAM_URL, 'External link to story from a feed', VALUE_OPTIONAL),
-                'classes' => new \external_value(PARAM_TEXT, 'Extra CSS classes', VALUE_OPTIONAL, ''),
-                'groupindication' => new \external_value(PARAM_RAW, 'Details of group-specific visibility', VALUE_OPTIONAL),
-                'hasattachments' => new \external_value(PARAM_BOOL, 'Does the message include attachments?', VALUE_OPTIONAL, false),
-                'attachments' => new \external_multiple_structure(
-                    new \external_single_structure([
-                        'filename' => new \external_value(PARAM_TEXT, 'Attachement filename'),
-                        'iconsrc' => new \external_value(PARAM_URL, 'Attachment icon URL'),
-                        'iconalt' => new \external_value(PARAM_TEXT, 'Attachement icon alt text'),
-                        'url' => new \external_value(PARAM_URL, 'Attachement download URL')
+                'imagedesc' => new external_value(PARAM_TEXT, 'Image description', VALUE_OPTIONAL, ''),
+                'eventdate' => new external_value(PARAM_TEXT, 'Short-form start date of event', VALUE_OPTIONAL),
+                'eventmonth' => new external_value(PARAM_ALPHA, 'Month of the event start date', VALUE_OPTIONAL),
+                'eventday' => new external_value(PARAM_ALPHANUM, 'Day of the event start date', VALUE_OPTIONAL),
+                'fulleventdate' => new external_value(PARAM_TEXT, 'Long-form start date of event', VALUE_OPTIONAL),
+                'eventlocation' => new external_value(PARAM_TEXT, 'Event location', VALUE_OPTIONAL),
+                'formattedmessage' => new external_value(PARAM_RAW, 'HTML formatted message text'),
+                'viewlink' => new external_value(PARAM_URL, 'Internal link to full message', VALUE_OPTIONAL),
+                'link' => new external_value(PARAM_URL, 'External link to story from a feed', VALUE_OPTIONAL),
+                'classes' => new external_value(PARAM_TEXT, 'Extra CSS classes', VALUE_OPTIONAL, ''),
+                'groupindication' => new external_value(PARAM_RAW, 'Details of group-specific visibility', VALUE_OPTIONAL),
+                'hasattachments' => new external_value(PARAM_BOOL, 'Does the message include attachments?', VALUE_OPTIONAL, false),
+                'attachments' => new external_multiple_structure(
+                    new external_single_structure([
+                        'filename' => new external_value(PARAM_TEXT, 'Attachement filename'),
+                        'iconsrc' => new external_value(PARAM_URL, 'Attachment icon URL'),
+                        'iconalt' => new external_value(PARAM_TEXT, 'Attachement icon alt text'),
+                        'url' => new external_value(PARAM_URL, 'Attachement download URL')
                     ], 'Attachment link', VALUE_OPTIONAL
                 ), 'Attachment links', VALUE_OPTIONAL)
             ]), VALUE_OPTIONAL),
-            'moremessages' => new \external_value(PARAM_INT,
+            'moremessages' => new external_value(PARAM_INT,
                     'Are there more messages to load? 0 for no, 1 for yes, 2 for yes but switch to past events.')
         ]);
     }
--- classes/local/external/get_courseid_from_messageid.php
+++ classes/local/external/get_courseid_from_messageid.php
@@ -16,11 +16,11 @@
 
 namespace block_news\local\external;
 
-use external_api;
-use external_function_parameters;
-use external_single_structure;
-use external_value;
 use block_news\system;
+use core_external\external_api;
+use core_external\external_function_parameters;
+use core_external\external_single_structure;
+use core_external\external_value;
 
 require_once($CFG->libdir . '/externallib.php');
 
